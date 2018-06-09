adf4945315fcc0d4a7fbf9cb2304b295bab70cf6
RonaldZhao <zzgdata@gmail.com>
2018-06-09 19:23:40 +0800

    almost done

---
+++ forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, PasswordField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('User Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('remember Me', default=False)
