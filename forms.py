c4612a705a40d450d542df8d6f27eb3e4e285582
RonaldZhao <zzgdata@gmail.com>
2018-06-11 00:05:42 +0800

    add RegisterForm

--- forms.py
+++ forms.py
@@ -4,6 +4,13 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('User Name', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('remember Me', default=False)
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    password_check = PasswordField(
+        'password_check', validators=[DataRequired()])
