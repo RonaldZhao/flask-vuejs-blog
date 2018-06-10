40871335732addd0aef6bf5205c660450d46cbec
RonaldZhao <zzgdata@gmail.com>
2018-06-11 02:39:42 +0800

    更改密码字段名

--- models.py
+++ models.py
@@ -7,12 +7,12 @@ from myapp import db
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(100), nullable=False)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __init__(self, username, password):
         self.username = username
-        self.password = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def __repr__(self):
         return '<User %r>' % self.username
