3b1ded973f66ede2875608e3b42679be5be99888
RonaldZhao <zzgdata@gmail.com>
2018-06-11 23:18:00 +0800

    优化表单设计

--- forms.py
+++ forms.py
@@ -1,16 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, PasswordField
+from wtforms import StringField, BooleanField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)
+    username = StringField(label='用户名', validators=[DataRequired()])
+    password = PasswordField(label='密码', validators=[DataRequired()])
+    remember_me = BooleanField(label='记住我', default=False)
+    submit = SubmitField(label='登录')
 
 
 class RegisterForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
-    password_check = PasswordField(
-        'password_check', validators=[DataRequired()])
+    username = StringField(label='用户名', validators=[DataRequired()])
+    password = PasswordField(label='密码', validators=[DataRequired()])
+    password_check = PasswordField(label='确认密码', validators=[DataRequired()])
+    submit = SubmitField(label='注册')
