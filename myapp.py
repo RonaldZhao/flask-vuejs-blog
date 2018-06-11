5c4aa98204df8b2dff096f349c39eca465bbd114
RonaldZhao <zzgdata@gmail.com>
2018-06-11 23:14:16 +0800

    修改表单页面的文件名

--- myapp.py
+++ myapp.py
@@ -48,7 +48,8 @@ def login():
             return render_template('index.html', current_user=current_user)
         else:
             flash('密码错误!')
-    return render_template('login_or_register.html', form=form)
+    # return render_template('login_or_register.html', form=form)
+    return render_template('form.html', form=form)
 
 
 @app.route('/logout')
@@ -66,45 +67,17 @@ def register():
         user = User.query.filter_by(username=form.username.data).first()
         if user is not None:
             flash('此用户已存在!')
-            return render_template('login_or_register.html', form=form)
+            # return render_template('login_or_register.html', form=form)
+            return render_template('form.html', form=form)
         elif form.password.data != form.password_check.data:
             flash('两次输入密码不一致!')
-            return render_template('login_or_register.html', form=form)
+            # return render_template('login_or_register.html', form=form)
+            return render_template('form.html', form=form)
         else:
             user = User(form.username.data, form.password.data)
             db.session.add(user)
             db.session.commit()
             flash('注册成功, 请登录!')
             return redirect(url_for('login'))
-    return render_template('login_or_register.html', form=form)
-# csrf = CsrfProtect()
-# csrf.init_app(app)
-#
-#
-# @app.route('/')
-# @app.route('/main')
-# @login_required
-# def main():
-#     return render_template('main.html', username=current_user.username)
-#
-#
-# @app.route('/login', methods=['POST', 'GET'])
-# def login():
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         username = request.form.get('username', None)
-#         password = request.form.get('password', None)
-#         remember_me = request.form.get('remember_me', False)
-#         user = User(username, password)
-#         if user.verify_password(password):
-#             login_user(user, remember=remember_me)
-#             return redirect(request.args.get('next') or
-#                             url_for('main', current_user=current_user))
-#     return render_template('login.html', form=form)
-#
-#
-# @app.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     return redirect(url_for('login'))
+    # return render_template('login_or_register.html', form=form)
+    return render_template('form.html', form=form)
