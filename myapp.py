aa5821d34f853db075ac34476f44d9e9782ca636
RonaldZhao <zzgdata@gmail.com>
2018-06-12 02:36:03 +0800

    整合Flask-Markdown

--- myapp.py
+++ myapp.py
@@ -1,27 +1,28 @@
 import os
 
 from flask import Flask, render_template, flash, redirect, url_for
-from flask_login import LoginManager, login_user, current_user, login_required,\
-    logout_user
+from flask_login import LoginManager, login_user, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash
 from flask_bootstrap import Bootstrap
+from flaskext.markdown import Markdown
 
-from forms import LoginForm, RegisterForm
+from forms import LoginForm, RegisterForm, PostForm
 
 
 app = Flask(__name__)
 app.secret_key = os.urandom(16)
 app.config.from_pyfile('config.py')
+bootstrap = Bootstrap(app)
+Markdown(app)
 db = SQLAlchemy(app)
 
-from models import User
+from models import User, Post
 
 login_manager = LoginManager()
-# login_manager.session_protection = 'strong'
-# login_manager.login_view = 'login'
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'login'
 login_manager.init_app(app=app)
-bootstrap = Bootstrap(app)
 
 
 @login_manager.user_loader
@@ -33,11 +34,14 @@ def load_user(user_id):
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    posts = Post.query.order_by(-Post.post_time)  # 查询所有已发布文章并根据发布时间逆序排列
+    return render_template('index.html', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:  # 如果用户已登录则直接跳转到首页
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -45,33 +49,34 @@ def login():
             flash('此用户不存在!')
         elif check_password_hash(user.password_hash, form.password.data):
             login_user(user)
-            return render_template('index.html', current_user=current_user)
+            return redirect(url_for('index'))
         else:
             flash('密码错误!')
-    # return render_template('login_or_register.html', form=form)
     return render_template('form.html', form=form)
 
 
 @app.route('/logout')
-@login_required
 def logout():
+    if not current_user.is_authenticated:
+        flash('请先登录!')
+        return redirect(url_for('login'))
     logout_user()
     flash('你已退出登录.')
-    return redirect(url_for('login'))
+    return redirect(url_for('index'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:  # 如果用户已登录则直接跳转到首页
+        return redirect(url_for('index'))
     form = RegisterForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is not None:
             flash('此用户已存在!')
-            # return render_template('login_or_register.html', form=form)
             return render_template('form.html', form=form)
         elif form.password.data != form.password_check.data:
             flash('两次输入密码不一致!')
-            # return render_template('login_or_register.html', form=form)
             return render_template('form.html', form=form)
         else:
             user = User(form.username.data, form.password.data)
@@ -79,5 +84,28 @@ def register():
             db.session.commit()
             flash('注册成功, 请登录!')
             return redirect(url_for('login'))
-    # return render_template('login_or_register.html', form=form)
     return render_template('form.html', form=form)
+
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    if not current_user.is_authenticated:
+        flash('请先登录!')
+        return redirect(url_for('login'))
+    form = PostForm()
+    if form.validate_on_submit():
+        # TODO: 文章名过长时的错误提示
+        post = Post(form.title.data, form.content.data)
+        post.author = current_user
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('post.html', form=form)
+
+
+@app.route('/pages/<int:post_id>')
+def pages(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    if not post:
+        return redirect(url_for('index'))
+    return render_template('page.html', post=post)
