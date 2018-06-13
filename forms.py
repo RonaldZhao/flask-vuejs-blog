b51f1f5cdb66bb85fde216db4eb1c929afb438aa
RonaldZhao <zzgdata@gmail.com>
2018-06-13 23:27:29 +0800

    alter username to user_name

--- forms.py
+++ forms.py
@@ -5,14 +5,14 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField(label='用户名', validators=[DataRequired()])
+    user_name = StringField(label='用户名', validators=[DataRequired()])
     password = PasswordField(label='密码', validators=[DataRequired()])
     remember_me = BooleanField(label='记住我', default=False)
     submit = SubmitField(label='登录')
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='用户名', validators=[DataRequired()])
+    user_name = StringField(label='用户名', validators=[DataRequired()])
     password = PasswordField(label='密码', validators=[DataRequired()])
     password_check = PasswordField(label='确认密码', validators=[DataRequired()])
     submit = SubmitField(label='注册')
